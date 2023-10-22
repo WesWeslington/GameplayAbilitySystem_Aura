f0b27bfd9288117906aea1dc81f2d3e15ef8f0cc
Wesley Gilbert <itswesleyg@gmail.com>
2023-10-22 10:46:09 -0400

    Lecture 13 - Enemy Interface

---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Created by Wes Gilbert through the Druid Mechanics Udemy Course
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
